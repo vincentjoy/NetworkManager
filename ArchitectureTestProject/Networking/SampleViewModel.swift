4f1bca3a74dda085edc59c0e04a0d9b17605e538
Vincent Joy <vincentjoyhere@gmail.com>
2025-02-06 13:16:12 +0530

    load core data

--- ArchitectureTestProject/Networking/SampleViewModel.swift
+++ ArchitectureTestProject/Networking/SampleViewModel.swift
@@ -1,12 +1,46 @@
 
 import Foundation
 import Combine
+import CoreData
 
 @Observable final class SampleViewModel {
+    
+    let container: NSPersistentContainer
+    var savedEntities: [Birds] = []
+    
     var response: [Birds]?
     var errorMessage: String?
     private var cancellables = Set<AnyCancellable>()
     private let networkManager = NetworkManager()
+    
+    init() {
+        container = NSPersistentContainer(name: "ArchitectureTestProject")
+        container.loadPersistentStores { [weak self] description, error in
+            if error == nil {
+                self?.loadData()
+            }
+        }
+    }
+    
+    func loadData() {
+//        let fetchRequest = NSFetchRequest<Birds>(entityName: "Birds")
+//        do {
+//            savedEntities = try container.viewContext.fetch(fetchRequest)
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+    }
+    
+    func saveData() {
+//        let fruitEntity = Birds(context: container.viewContext)
+//        fruitEntity.name = "Test"
+//        do {
+//            try container.viewContext.save()
+//            loadData()
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+    }
     
     func fetchExampleData() {
         networkManager.combineRequest(pathUrl: .birds, httpMethod: .GET, resultType: [Birds].self)
